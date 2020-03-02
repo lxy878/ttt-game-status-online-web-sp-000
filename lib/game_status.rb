0a88f48fe8c91208185b29787ab5766c9a5291e9
Xiaoyi Liu <lxy878@gmail.com>
2020-03-02 07:01:30 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -13,7 +13,7 @@ def won(board)
 
 
 
-  WIN_COMBINATIONS.select do |win|
+  win = WIN_COMBINATIONS.select do |win|
     taken = win.all? {|postion| position_taken?(board, postion)}
     if taken and (board[win[0]] == board[win[1]] and board[win[1]] == board[2])
       return win
